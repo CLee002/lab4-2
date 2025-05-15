56a74caf8cd6cfa1ba20f3dc4bb6baaab94c176f
Charles Lee <leecf2@miamioh.edu>
2025-05-14 21:41:12 -0400

    modified:   data-gen.py

--- data-gen.py
+++ data-gen.py
@@ -1,5 +1,4 @@
 import sqlite3
-from random_address import real_random_address
 import os
 
 DATABASE = '/nfs/demo.db'
@@ -14,8 +13,7 @@ def generate_test_data(num_contacts):
     for i in range(num_contacts):
         name = f'Test Name {i}'
         phone = f'123-456-789{i}'
-        t = real_random_address()
-        address = f'({t}{i})'
+        address = f'({1000/i} Zachary Drive{i})'
         db.execute('INSERT INTO contacts (name, phone, address) VALUES (?, ?, ?)', (name, phone, address))
     db.commit()
     print(f'{num_contacts} test contacts added to the database.')
